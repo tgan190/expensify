a1541c0de72c2f61ef8e81ec98855d47784a71cc
tgan190 <teowgan@gmail.com>
2017-10-23 15:08:13 +1100

    setup firebase and test database environment

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
 import ExpenseListFilters from './ExpenseListFilters';
-import ShowExpenseTotal from './ShowExpenseTotal';
+import ExpensesSummary from './ExpensesSummary';
 
 const ExpenseDashboardPage = () => (
   <div>
-    <ShowExpenseTotal />
+    <ExpensesSummary />
     <ExpenseListFilters />
     <ExpenseList />
   </div>
--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -1,7 +1,11 @@
 // required to support v16 of react
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import DotEnv from 'dotenv';
 
 Enzyme.configure({
     adapter: new Adapter()
 })
+
+// require('dotenv').config({ path: '.env.test' });
+DotEnv.config({ path: '.env.test' });
